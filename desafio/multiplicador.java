1712d5c14974368e5264795409f96fa638c51342
Cicero Antonio Ferreria Ferreira <ciceroferreira2010@hotmail.com>
2021-02-19 16:01:53 -0300

    completando exercicio

--- desafio/multiplicador.java
+++ desafio/multiplicador.java
@@ -13,14 +13,44 @@ public class Multiplicador {
 		
 		public static boolean verificarInput(int multiplicando, int inicio, int fim) {
 			
-			if(multiplicando < 0|| multiplicando > 3000) {
-				System.out.println("O multipicando não pode ser negativo e não pode ser maior que 3000");
-					return false;
-			}
+			 if(verificarInput(multiplicando, inicio, fim)) {
+	    	  System.out.println("multiplicando:" + multiplicando);
+	    	  System.out.println("Início do intervalo:" + inicio);
+	    	  System.out.println("fim do intervalo:" + fim);
+	    	  
+	    	  for(int i = inicio; i <= fim ; i++) {
+	    		  System.out.println(multiplicando + "x" + i + "=" + multiplicando * i );
 			
-			return null;
+			
 		}
 		
+	}
+
+}
+public static Boolean verificarInput(int multiplicando, int inicio, int fim) {
+		
+		if(multiplicando < 0) {
+			System.out.println("O multiplicando não pode ser negativo");
+		
+		} else if(multiplicando > 3000) {
+			System.out.println("O multiplicando não pode ser maior que 3000");
+			
+		} else if(inicio < 0 || fim < 0) {
+			System.out.println(" Os intervalos não  podem  ser  números negativos");
+			
+		} else if(inicio > 3000 || fim > 3000) {
+			
+			System.out.println(" Os intervalos não  podem  ser  maior que 3000");
+		} else if (inicio > fim) {
+			System.out.println("intervalos invalidos");
+		} else if (fim - inicio > 10) {
+			System.out.println("intervalos invalidos");
+		} else {
+			return true;
+		}
+		
+
+		return false;
 	}
 
 }
